9e3bedf35d094af01b5306881168c6585b1c431c
Md.Ghulam Azad Ansari <iamghulamazad@gmail.com>
2022-01-10 06:13:49 +0530

    deleted old py files

--- codes/Day-099/currency-converter/main.py
+++
@@ -1,8 +0,0 @@
-from currency_converter import CurrencyConverter
-from app import App
-
-
-if __name__ == '__main__':
-    url = 'https://api.exchangerate-api.com/v4/latest/USD'
-    converter = CurrencyConverter(url)
-    App(converter).mainloop()
