0df5747041635c470abd591d8df89f0bda826740
Md.Ghulam Azad Ansari <iamghulamazad@gmail.com>
2022-01-14 06:17:53 +0530

    deleted js file

--- codes/Day-091/PDF_to_Audiobook/static/js/app.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  $(document).on("change", ":file", function () {
-    let input = $(this);
-    let filename = input.val().replace(/\\/g, "/").replace(/.*\//, "");
-    let parentInput = $(this).parents(".input-group").find(":text");
-    parentInput.val(filename);
-  });
-});
