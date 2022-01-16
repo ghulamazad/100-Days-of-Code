d73e8f427dead91a11c9baa7b281c79a9fa3d923
MdGhulamAzadAnsari <Md.GhulamAzadAnsari786@gmail.com>
2022-01-16 12:10:28 +0530

    added docs

---
+++ docs/js/script.js
@@ -0,0 +1,33 @@
+const getCard = (name, description, link) => {
+  var newNode = document.createElement("div");
+  newNode.className = "col-xl-3 col-sm-6 col-12";
+  newNode.innerHTML = `<div class="card">
+                                <div class="card-body">
+                                <h5 class="card-title">${name}</h5>
+                                <p class="card-text">
+                                ${description}
+                                </p>
+                                <a href="${link}" class="btn btn-primary">Source Code</a>
+                                </div>
+                            </div>`;
+  return newNode;
+};
+
+const displayProjects = (projects) => {
+  const cardWrapper = document.getElementById("card-wrapper");
+  projects.forEach((project) => {
+    cardWrapper.appendChild(
+      getCard(project.name, project.description, project.link)
+    );
+  });
+};
+
+// Fetch data from json file
+const projectJsonFile =
+  "https://raw.githubusercontent.com/MdGhulamAzadAnsari/100-Days-of-Code/master/docs/data/projects.json";
+
+fetch(projectJsonFile)
+  .then((data) => data.json())
+  .then((data) => {
+    displayProjects(data.projects);
+  });
