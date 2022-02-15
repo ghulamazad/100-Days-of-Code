99e3b778c06539a7e8d56407a64095f73d633504
Md.Ghulam Azad Ansari <iamghulamazad@gmail.com>
2022-02-15 06:23:18 +0530

    update :)

---
+++ codes/Day-095/currency-converter/currency_converter.py
@@ -0,0 +1,16 @@
+import requests
+
+
+class CurrencyConverter():
+    def __init__(self, url):
+        self.data = requests.get(url).json()
+        self.currencies = self.data['rates']
+
+    def convert(self, from_currency, to_currency, amount):
+        initial_amount = amount
+        if from_currency != 'USD':
+            amount = amount / self.currencies[from_currency]
+
+        # limiting the precision to 4 decimal places
+        amount = round(amount * self.currencies[to_currency], 4)
+        return amount
